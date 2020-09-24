195e99cf8b10bf25279a223f5897abbc74c40bf3
Mateus <55644123+mateusmmj2010@users.noreply.github.com>
2020-09-23 22:01:07 -0300

    commit 3.2

--- src/controller/Listc.java
+++ src/controller/Listc.java
@@ -3,106 +3,177 @@ package controller;
 public class Listc {
 	/**
 	 *  FALTA DOCUMENTAR CLASSE
-	 */
-	
+	 */	
 	private Listc firstItem, lastItem;
 	private Listc prevItem, nextItem;
 	private double temperatura;
-	private int length;
+	private int length = 0;
 	
 	public Listc() {
 		super();
 	}
 	
-	public void appendList (double temp) {
-		Listc no = new Listc();
-		no.temperatura = temp;
-		
-		if (this.firstItem == null) {
-			this.firstItem = no;
-			this.lastItem = no;		
-			this.length ++;
+	public boolean emptyList() {
+		/**
+		 *  this methods retunrs TRUE if list if first item is null
+		 */
+		try {
+			if (firstItem == null) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			String erro = e.getMessage();
+			System.out.println("exceção não tratada" + erro);
+			return false;
 		}
-		else {
-			Listc aux = new Listc();
-			aux = this.lastItem;
-			aux.nextItem = no;
-			this.lastItem = no;
-			this.lastItem.prevItem = aux;
-			this.length ++;
-		}		
+	}	
+	
+
+	public void inserList(double value, int position) {
+		/**
+		 *  this methods insert a element acording a indice
+		 */
+		try {		
+			Listc no = new Listc();
+			no.temperatura = value;
+		
+			if ((this.length < 1 || position >= this.length) && position >= 0 ) {
+				appendList(value);
+			}
+		
+			
+			else if (position > this.length || position < 0) {
+				System.out.println("inserção inválida, posição inexistente");
+			}
+
+			else {
+			
+				if (position == 0) {
+					insertInicio(no);			
+				}
+
+				else {				
+					insertPosition(value, position, 0, no, this.firstItem);	
+				}
+			}		
+		} catch (Exception e) {
+			System.out.println("inclusão inválida");
+		}
+	}
+	
+	public void insertInicio(Listc no) {
+		/**
+		 *  this methods insert elements in indice 0
+		 */
+		try {
+			Listc auxA = new Listc();
+			auxA = this.firstItem;
+			auxA.prevItem = this.firstItem ;
+			this.firstItem = no;
+			this.firstItem.nextItem = auxA;
+			this.firstItem.prevItem = null;			
+			this.length++;
+		} catch (Exception e) {
+			System.out.println("inclusão inválida");
+		}
+	}	
+	
+	public void insertPosition(double value, int position, int contador, Listc no, Listc itemCompare) {	
+		/**
+		 * this methods insert element in indice != 0 && != list.length
+		 */
+		try {
+			if (position == contador) {
+				no.prevItem = itemCompare.prevItem;
+				no.nextItem = itemCompare;
+				no.nextItem.prevItem = no;
+				no.prevItem.nextItem = no;
+				this.length ++;		
+			}
+			else {
+				insertPosition(value, position, contador+1,no, itemCompare.nextItem);
+			}		
+		} catch(Exception e) {
+			System.out.println("inclusão inválida");
+		}
+	}
+	
+	public void appendList (double temp) {
+		/**
+		 * tis methods add item on final of list
+		 */
+		try {
+			Listc no = new Listc();
+			no.temperatura = temp;
+		
+			if (this.firstItem == null) {
+				this.firstItem = no;
+				this.lastItem = no;		
+				this.length ++;
+			}
+			else {
+				Listc aux = new Listc();
+				aux = this.lastItem;
+				aux.nextItem = no;
+				this.lastItem = no;
+				this.lastItem.prevItem = aux;
+				this.length ++;
+			}		
+		} catch(Exception e) {
+			System.out.println("inclusão inválida");
+		}
 	}
 	
 	public void popList() {
+		/**
+		 * this methods remove last item
+		 */
 		try {
 			this.lastItem = this.lastItem.prevItem;
 			this.lastItem.nextItem = null;
 			this.length --;
-		} catch (NullPointerException e) {
+		}catch (NullPointerException e) {
 			System.out.println("lista vazia");
 		}
-	}	
-	
-	public void inserList(double value, int position) {
-		Listc no = new Listc();
-		no.temperatura = value;
-		
-		if (this.length < 1 || position == this.length ) {
-			appendList(value);
-		}
-		
-		else if (position > this.length) {
-			System.out.println("inserção inválida, posição inexistente");
-		}
-		
-		else {
-			
-			if (position == 0) {
-				Listc noA = new Listc();
-				noA.temperatura = value;	
-				
-				Listc aux = new Listc();
-				aux = this.firstItem;
-				aux.prevItem = this.firstItem ;
-				this.firstItem = no;
-				this.firstItem.nextItem = aux;
-				this.firstItem.prevItem = null;			
-				length++;
-				
-				
-			}
-
-			else {
-				Listc auxA = this.firstItem;
-				int auxB = 0;
-				int auxC = 0;
-				
-				while (auxC == 0) {
-					if (auxB == position) {
-						no.prevItem = auxA.prevItem;
-						no.nextItem = auxA;
-						no.nextItem.prevItem = no;
-						no.prevItem.nextItem = no;
-						auxC ++;
-						this.length ++;
-					}
-					else {
-						auxB ++;
-						auxA = auxA.nextItem;
-					}				
-				}				
-			}
-		}		
 	}
 	
+	public void popFirstList() {
+		/**
+		 * this methods remove first item
+		 */
+		try {
+			if (this.length == 1) {
+				this.firstItem = null;
+				this.length --;
+			} else if (this.length > 1){ 
+				this.firstItem = this.firstItem.nextItem;
+				this.firstItem.prevItem = null;
+				this.length --;
+			} else {
+				System.out.println("lista vazia");
+			}			
+			
+		} catch(Exception e) {
+			System.out.println("lista vazia");
+		}
+		
+	}
 	
 	public void showList() {
-		Listc aux = this.firstItem;
-		
-		do {
-			System.out.println("item "+aux.temperatura);
-			aux = aux.nextItem;
-		} while (aux != null);	
-		System.out.print(length);
+		/**
+		 * this methods shows all items on list
+		 */
+		try {
+			Listc aux = this.firstItem;
+			do {
+				System.out.println("item "+aux.temperatura);
+				aux = aux.nextItem;
+			} while (aux != null);	
+			System.out.print(length);
+		} catch (NullPointerException e) {
+			System.out.println("item não pode ser exibido");			
+		}
 	}
 }
--- src/main/main.java
+++ src/main/main.java
@@ -8,9 +8,21 @@ public class main {
 //		Stack newStack = new Stack(); //test stack	
 //		Queue newQueue = new Queue(); //test queue
 		Listc newList = new Listc(); //test list
-		newList.inserList(10,1);
-		newList.inserList(20,1);
+		
+		
+		newList.inserList(15,0);
+		newList.inserList(17,1);
+		newList.popFirstList();
+		newList.inserList(10,2);
+		newList.popList();
+		newList.inserList(20,3);
+		newList.inserList(30,4);
+		newList.inserList(5,5);
 		newList.showList();
+
+		// TRATAMENTO DE EXCEÇÃO NA CHAMADA DO METODO  - para null ou tipo diferente
+
+		
 		
 	}
 }
