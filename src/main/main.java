527d7401893655bb5a15f11594105c91823d9154
Mateus <55644123+mateusmmj2010@users.noreply.github.com>
2020-09-24 17:59:39 -0300

    commit 5.0

--- src/main/main.java
+++ src/main/main.java
@@ -8,23 +8,13 @@ public class main {
 	public static void main(String[] args) {
 //		Stack newStack = new Stack(); //test stack	
 //		Queue newQueue = new Queue(); //test queue
-     	Listc novo = new Listc(); //test list
-//		ListAlumns novo = new ListAlumns();
-//		
-//		novo.inserList(10, "m", "a", "b", 0);
-//		novo.inserList(10, "m", "b", "b", 0);
-//		novo.inserList(10, "m", "c", "b", 1);
-//		
-     	novo.inserList(50, 0);
-     	novo.inserList(60, 0);
-     	novo.inserList(70, 1);
-     	novo.showList();
+//     	Listc novoA = new Listc(); //test list
+//		ListAlumns novoB = new ListAlumns();
+		ListBooks novoC = new ListBooks();
 		
-     	
-		
-		// TRATAMENTO DE EXCEÇÃO NA CHAMADA DO METODO  - para null ou tipo diferente
+
 
 		
-		
+	
 	}
 }
