0570a017f659eeb9d8ccb0702be484373bbb45a1
Mateus <55644123+mateusmmj2010@users.noreply.github.com>
2020-09-25 22:42:25 -0300

    Update ListAlumns.java

--- src/controller/ListAlumns.java
+++ src/controller/ListAlumns.java
@@ -1,222 +1,199 @@
 package controller;
 
-public class ListAlumns {
-	/**
-	 *  FALTA DOCUMENTAR CLASSE - ALUNOS - EXERCICIO 3
-	 */	
-	public ListAlumns firstItem, lastItem;
-	private ListAlumns prevItem, nextItem;
-	private int RaAlumn;
-	private String nome, turma, semestre;
-	private int length = 0;
+public class listaAluno {
+	private static int lengthListaAluno;
+	private int raAluno;
+	private String nomeAluno, turmaAluno, semestreAluno;
+	private listaAluno primeiroAluno, ultimoAluno;
+	private listaAluno prevAluno, proxAluno;	
 	
-	public ListAlumns() {
+	public listaAluno() {
 		super();
 	}
 	
-	public boolean emptyList() {
-		/**
-		 *  this methods returns TRUE if list if first item is null
-		 */
-		try {
-			if (firstItem == null) {
-				return true;
-			} else {
-				return false;
-			}
-		} catch (Exception e) {
-			String erro = e.getMessage();
-			System.out.println("exceÁ„o n„o tratada" + erro);
-			return false;
-		}
+	public listaAluno(int raAluno, String nomeAluno, String turmaAluno, String semestreAluno) {
+		this.raAluno = raAluno;
+		this.nomeAluno = nomeAluno;
+		this.turmaAluno = turmaAluno;
+		this.semestreAluno = semestreAluno;
+		this.prevAluno = null;
+		this.proxAluno = null;
+		this.primeiroAluno = this;
+		this.ultimoAluno = this;		
 	}	
 	
-
-	public void inserList(int RaAlumn, String nome, String turma, String semestre, int position) {
+	public void appendInicio(int raAluno, String nomeAluno, String turmaAluno, String semestreAluno) {
 		/**
-		 *  this methods insert a element acording a indice
-		 */
-		try {		
-			ListAlumns no = new ListAlumns();
-			no.RaAlumn = RaAlumn;
-			no.nome = nome;
-			no.turma = turma;
-			no.semestre = semestre;			
+		 * adiciona inicio
+		 */		
+		listaAluno no = new listaAluno(raAluno, nomeAluno, turmaAluno, semestreAluno);
 		
-			if (this.length < 1 && position == this.length && position >= 0 ) {
-				appendList(no);
-			}		
-			
-			else if (position > this.length || position < 0) {
-				System.out.println("inserÁ„o inv·lida, posiÁ„o inexistente");
+		if (this.primeiroAluno == null) {
+			this.primeiroAluno = no;
+			this.ultimoAluno = no;
+			lengthListaAluno ++;
+		}
+		
+		else {
+			listaAluno aux = new listaAluno();
+			aux = this.primeiroAluno;
+			aux.prevAluno = no;
+			no.proxAluno = aux;
+			this.primeiroAluno = no;
+			lengthListaAluno ++;			
+		}		
+	}
+	
+	
+	public void removeInicio() {
+		/**
+		 * remove inicio
+		 */
+		if (this.primeiroAluno == null) {
+			System.out.println("remo√ß√£o inv√°lida, lista vazia");
+		}
+		else {
+			this.primeiroAluno = this.primeiroAluno.proxAluno;
+			this.primeiroAluno.prevAluno = null;
+			lengthListaAluno --;
+		}		
+	}
+	
+	
+	public void appendFinal(int raAluno, String nomeAluno, String turmaAluno, String semestreAluno) {
+		/**
+		 * adiciona no final
+		 */
+		if (this.primeiroAluno == null) {
+			appendInicio(raAluno, nomeAluno, turmaAluno, semestreAluno);
+		}
+		else {
+			listaAluno no = new listaAluno(raAluno, nomeAluno, turmaAluno, semestreAluno);
+			listaAluno aux = new listaAluno();			
+			aux = this.ultimoAluno;
+			aux.proxAluno = no;
+			no.prevAluno = aux;
+			this.ultimoAluno = no;
+			lengthListaAluno ++;
+		}		
+	}
+	
+	public void removeFinal() {
+		if (this.primeiroAluno == null) {
+			System.out.println("remo√ß√£o inv√°lida, lista vazia");
+		}
+		else {
+			this.ultimoAluno = this.ultimoAluno.prevAluno;
+			this.ultimoAluno.proxAluno = null;
+			lengthListaAluno --;
+		}
+	}
+	
+	public void appendPosition(int raAluno, String nomeAluno, String turmaAluno, String semestreAluno, int position) {
+		listaAluno no = new listaAluno(raAluno, nomeAluno, turmaAluno, semestreAluno);
+		appendPosition(no, this.primeiroAluno, position, 0);
+	}
+	
+	
+	private void appendPosition(listaAluno no, listaAluno compare, int position, int contador) {
+		/**
+		 * RECURSIVIDADE DIRETA
+		 * este metodo adiciona um item a alguma posicao da lista,
+		 * para encontrar a posicao correta o metodo √© chamado recursivamente, os parametros "compare" e "contador" s√£o incrementados
+		 * compare do tipo listaAluno, recebe compare.proxAluno - contador receber contador + 1		 * 
+		 */
+		
+		try {
+			if (position == 0) {
+				appendInicio(no.raAluno, no.nomeAluno, no.turmaAluno, no.semestreAluno);			
 			}
-
-			else {
+			else if (position == lengthListaAluno) {
+				appendFinal(no.raAluno, no.nomeAluno, no.turmaAluno, no.semestreAluno);
+			}
 			
-				if (position == 0) {
-					insertInicio(no);			
+			else {
+				if (position == contador) {
+					no.prevAluno = compare.prevAluno;
+					no.proxAluno = compare;
+					no.prevAluno.proxAluno = no;
+					no.proxAluno.prevAluno = no;
+					lengthListaAluno ++;				
 				}
-
-				else {				
-					insertPosition(position, 0, no, this.firstItem);	
-				}
-			}		
-		} catch (Exception e) {
-			System.out.println("inclus„o inv·lida A");
-		}
-	}
-	
-	
-	public void removeList(int position) {
-
-		try {
-			if (this.length < 1 || position > this.length){
-				System.out.println("remoÁ„o inv·lida");
+				else {		
+					// chamada recursiva
+					appendPosition(no, compare.proxAluno, position, contador+1);
+				}				
 			}
-	
-			else if (position == 0){
-				popFirstList();
-			}
-	
-			else if (position == this.length-1){
-				popList();
-			}
-	
-			else{
-				removePosition(position, 0, this.firstItem);
-			}
-		} catch (Exception e){
-			System.out.println("remoÁ„o inv·lida");
-		}
-		
-	}
-	
-	public void insertInicio(ListAlumns aluno) {
-		/**
-		 *  this methods insert elements in indice 0
-		 */
-		try {
-			ListAlumns auxA = new ListAlumns();
-			auxA = this.firstItem;
-			auxA.prevItem = this.firstItem ;
-			this.firstItem = aluno;
-			this.firstItem.nextItem = auxA;
-			this.firstItem.prevItem = null;			
-			this.length++;
-		} catch (Exception e) {
-			System.out.println("inclus„o inv·lida B");
-		}
-	}	
-	
-	public void insertPosition(int position, int contador, ListAlumns no, ListAlumns itemCompare) {	
-		/**
-		 * this methods insert element in indice != 0 && != list.length
-		 */
-		try {
-			if (position == contador) {
-				no.prevItem = itemCompare.prevItem;
-				no.nextItem = itemCompare;
-				no.nextItem.prevItem = no;
-				no.prevItem.nextItem = no;
-				this.length ++;		
-			}
-			else {
-				insertPosition(position, contador+1,no, itemCompare.nextItem);
-			}		
-		} catch(Exception e) {
-			System.out.println("inclus„o inv·lida C");
-		}
-	}
-	
-	public void removePosition(int position, int contador, ListAlumns itemCompare) {
-		try {
-			if(position == contador) {				
-				itemCompare.nextItem.prevItem = itemCompare.prevItem;
-				itemCompare.prevItem.nextItem = itemCompare.nextItem;
-				itemCompare = null;
-				this.length --;
-			}
-			else {
-				System.out.println("aqui" + position + contador);
-				removePosition(position, contador+1,itemCompare.nextItem);
-			}
-		} catch (Exception e) {
-			System.out.println("remoÁ„o inv·lida");
-		}
-	}
-	
-	
-	public void appendList (ListAlumns aluno) {
-		/**
-		 * tis methods add item on final of list
-		 */
-		try {
-			if (this.firstItem == null) {
-				this.firstItem = aluno;
-				this.lastItem = aluno;		
-				this.length ++;
-			}
-			else {
-				ListAlumns aux = new ListAlumns();
-				aux = this.lastItem;
-				aux.nextItem = aluno;
-				this.lastItem = aluno;
-				this.lastItem.prevItem = aux;
-				this.length ++;
-			}		
-		} catch(Exception e) {
-			System.out.println("inclus„o inv·lida");
-		}
-	}
-	
-	public void popList() {
-		/**
-		 * this methods remove last item
-		 */
-		try {
-			this.lastItem = this.lastItem.prevItem;
-			this.lastItem.nextItem = null;
-			this.length --;
-		}catch (NullPointerException e) {
-			System.out.println("lista vazia");
-		}
-	}
-	
-	public void popFirstList() {
-		/**
-		 * this methods remove first item
-		 */
-		try {
-			if (this.length == 1) {
-				this.firstItem = null;
-				this.length --;
-			} else if (this.length > 1){ 
-				this.firstItem = this.firstItem.nextItem;
-				this.firstItem.prevItem = null;
-				this.length --;
-			} else {
-				System.out.println("lista vazia");
-			}			
 			
-		} catch(Exception e) {
-			System.out.println("lista vazia");
-		}
-		
+		} catch (Exception e) {
+			System.out.println("inclus√£o inv√°lida");
+		}		
 	}
 	
-	public void showList() {
+	
+	public void removePosition(int posicao) {
 		/**
-		 * this methods shows all items on list
+		 * RECURSIVIDADE DIRETA
+		 * este metodo remove um item de alguma posicao da lista,
+		 * para encontrar a posicao correta o metodo √© chamado recursivamente, os parametros "compare" e "contador" s√£o incrementados
+		 * compare do tipo listaAluno, recebe compare.proxAluno - contador receber contador + 1		 * 
+		 */
+		removePosition(this.primeiroAluno, posicao, 0);
+	}
+	
+	private void removePosition(listaAluno compare, int position, int contador) {
+		try {
+			if (position == 0) {
+				removeInicio();
+			}
+			
+			else if (position == lengthListaAluno) {
+				removeFinal();
+			}
+			
+			else {
+				if(position == contador) {
+					compare.prevAluno.proxAluno = compare.proxAluno;
+					compare.proxAluno.prevAluno = compare.prevAluno;
+					compare = null;
+				}
+				else {
+					// chamada recursiva
+					removePosition(compare.proxAluno, position, contador+1);
+				}
+			}
+		} catch(Exception e) {
+			System.out.println("remo√ß√£o inv√°lida");
+		}
+	}
+	
+	
+	public void showListaAluno() {
+		/**
+		 * chamar showListaAluno
+		 */
+		showListaAluno(this.primeiroAluno);
+	}
+	
+	private void showListaAluno(listaAluno compare) {
+		/**
+		 * RECURSIVIDADE DIRETA
+		 * este metodo apresenta todos os itens da lista
+		 * o metodo √© chamado recursivamente - o parametro compare do tipo listaAluno √© incrementado, recebendo compara.proxAluno
+		 * 
 		 */
 		try {
-			ListAlumns aux = this.firstItem;
-			do {
-				System.out.println("nome "+aux.nome+" ra "+aux.RaAlumn+" sala "+aux.turma+" semeste "+aux.semestre);
-				aux = aux.nextItem;
-			} while (aux != null);	
-			System.out.print(length);
-		} catch (NullPointerException e) {
-			System.out.println("item n„o pode ser exibido");			
+			if (compare != null) {
+				System.out.println("aluno: "+compare.nomeAluno+" ra: "+compare.raAluno+
+						" turma: "+compare.turmaAluno+" semestre "+compare.semestreAluno);
+				
+				showListaAluno(compare.proxAluno);
+			}
+			else {
+				System.out.println("tamanho: "+lengthListaAluno);
+			}
+		} catch (Exception e) {
+			System.out.println("lista vazia");
 		}
 	}
 }
